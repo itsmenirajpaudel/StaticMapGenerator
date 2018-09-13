b8c8c74e4bf5ecaf581c39d35eb2745087f40f92
Niraj Paudel <itsmenirajpaudel@gmail.com>
2018-09-13 14:19:22 +0545

    changes made

--- src/components/contents/MapContent.js
+++ src/components/contents/MapContent.js
@@ -1,16 +1,28 @@
 import React from "react";
 
 const MapContent = function(props) {
+
+    let imageElem = (
+        <img
+        border="0"
+        onError={props.onImageError}
+        onLoad={props.onImageLoad}
+        src={props.model.url}
+        alt="api key is either empty or invalid"
+    />
+    ); 
+
+    if(props.invalidKey) {
+       imageElem = (
+            <p> api key is either empty or invalid </p>
+       ); 
+    } 
+
     return (
         <div className="posts">
             <h1 className="content-subhead">Map Preview</h1>
             <section className="post">
-                <img
-                    onError={props.onImageError}
-                    onLoad={props.onImageLoad}
-                    src={props.model.url}
-                    alt="api key is either empty or invalid"
-                />
+               {imageElem}
             </section>
         </div>
     );
